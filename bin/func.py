4546fb3229ae37df5cd393841c4b92a6eab1ed8a
StephenKinahan <c13407902@mydit.ie>
2016-10-22 15:33:10 +0100

    ud, dt, exit commands edit. user input handling changed

--- bin/func.py
+++ bin/func.py
@@ -1,19 +1,24 @@
-import subprocess
+import subprocess, os, time, getpass, grp, pwd
 import os
+import time
+import getpass
 
-def pwd(*args):
+def pw(*args):
     subprocess.call("pwd",shell=True)
 
 def ifc(*args):
     cmd = "ifconfig"
-    print(args)
-    if args[0] == []:
+    if args[1] == []:
         cmd += " eth0"
     else:
-        cmd += " "+str(*args[0])
-    print(cmd)
+        cmd += " "+str(*args[1])
     subprocess.call(cmd,shell=True)
-
+def dt(*args):
+    print time.strftime('%Y%m%d%H%M%S')
+def ud(*args):
+    name = getpass.getuser()
+    user = pwd.getpwnam(name)
+    print str(user.pw_uid)+","+str(user.pw_gid)+","+name+","+str(grp.getgrgid(user.pw_gid))+","+str(os.stat(os.getenv('HOME')).st_ino)
 def ls(*args):
     subprocess.call("ls",shell=True)
 
--- bin/shell.py
+++ bin/shell.py
@@ -9,16 +9,24 @@ while True:
     userinputsplit = userinput.split()
 
     if userinputsplit[0] == "pw":
-        func.pwd(userinputsplit[1:])
+        func.pw(userinputsplit)
 
     elif userinputsplit[0] == "ls":
-        func.ls(userinputsplit[1:])
+        func.ls(userinputsplit)
 
     elif userinputsplit[0] == "cd":
-        func.cd(userinputsplit[1:])
+        func.cd(userinputsplit)
 
     elif userinputsplit[0] == "ifc":
-        func.ifc(userinputsplit[1:])
+        func.ifc(userinputsplit)
 
+    elif userinputsplit[0] == "dt":
+        func.dt(userinputsplit)
+
+    elif userinputsplit[0] == "ud":
+        func.ud(userinputsplit)
+
+    elif userinputsplit[0] == "exit":
+        break
     else:
         print("Invalid command")
