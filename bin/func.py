8d1d9d0edd13a4678176a59a90e3478296779353
StephenKinahan <c13407902@mydit.ie>
2016-10-23 12:40:37 +0100

    user input handling changed

--- bin/func.py
+++ bin/func.py
@@ -8,19 +8,22 @@ def pw(*args):
 
 def ifc(*args):
     cmd = "ifconfig"
-    if args[1] == []:
+    if args[0][1] == []:
         cmd += " eth0"
     else:
-        cmd += " "+str(*args[1])
+        cmd += " "+str(args[0][1])
     subprocess.call(cmd,shell=True)
+
 def dt(*args):
     print time.strftime('%Y%m%d%H%M%S')
+
 def ud(*args):
     name = getpass.getuser()
     user = pwd.getpwnam(name)
     print str(user.pw_uid)+","+str(user.pw_gid)+","+name+","+str(grp.getgrgid(user.pw_gid))+","+str(os.stat(os.getenv('HOME')).st_ino)
+
 def ls(*args):
     subprocess.call("ls",shell=True)
 
 def cd(*args):
-    os.chdir(*args[0])
+    os.chdir(args[0][1])
